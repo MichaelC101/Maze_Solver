2893dae8c249febbe15af413e0c195c9465323f7
Sam Lonneman <56888420+SamLonneman@users.noreply.github.com>
2021-11-17 20:56:31 -0500

    Create printable matrix from adjList

--- Maze_Generator.cpp
+++ Maze_Generator.cpp
@@ -5,12 +5,35 @@
 #include <unordered_set>
 using namespace std;
 
-void printGraph(vector<vector<int>>& adjList)
+vector<vector<bool>> toMatrix(vector<unordered_set<int>>& adjList, int H, int W)
 {
+    vector<vector<bool>> matrix(H * 2 + 1, vector<bool>(W * 2 + 1, false));
     for (int i = 0; i < adjList.size(); i++) {
-        cout << i << ": ";
-        for (int j = 0; j < adjList[i].size(); j++) {
-            cout << adjList[i][j] << " ";
+        int r = i / W;
+        int c = i % W;
+        int row = r * 2 + 1;
+        int col = c * 2 + 1;
+        matrix[row][col] = true;
+        if (c < W - 1 && adjList[i].count(i + 1))
+            matrix[row][col + 1] = true;
+        if (r < H - 1 && adjList[i].count(i + W))
+            matrix[row + 1][col] = true;
+    }
+    matrix[1][0] = true;
+    matrix[H * 2 - 1][W * 2] = true;
+    return matrix;
+}
+
+void printMatrix(vector<vector<bool>>& matrix)
+{
+    char path = 255u;
+    char wall = 219u;
+    for (auto v : matrix) {
+        for (auto b : v) {
+            if (b)
+                cout << path << path;
+            else
+                cout << wall << wall;
         }
         cout << endl;
     }
@@ -18,41 +41,45 @@ void printGraph(vector<vector<int>>& adjList)
 
 int main()
 {
-    const int H = 5, W = 5;
-    vector<vector<int>> adjList(H * W);
+    const int H = 20, W = 20;
+    vector<unordered_set<int>> adjList(H * W);
     vector<bool> v(H * W);
     stack<pair<int,int>> s;
     s.emplace(0, -1);
+
+    // Should visit each node once
     while (!s.empty()) {
-        auto u = s.top();
+        int u = s.top().first;
+        int from = s.top().second; 
         s.pop();
-        if (v[u.first])
+        if (v[u])
             continue;
-        v[u.first] = true;
-        cout << u.first << endl;
-        if (u.second != -1) {
-            adjList[u.second].push_back(u.first);
-            adjList[u.first].push_back(u.second);
+        v[u] = true;
+        if (from != -1) {
+            adjList[from].insert(u);
+            adjList[u].insert(from);
         }
+
+        // Create list of unvisited neighbors
         vector<int> neighbors;
-        // If u has unvisited north neighbor, push north neighbor to neighbors
-        if (u.first / W > 0 && !v[u.first - W])
-            neighbors.push_back(u.first - W);
-        // If u has unvisited east neighbor, push east neighbor to neighbors
-        if (u.first % W < W - 1 && !v[u.first + 1])
-            neighbors.push_back(u.first + 1);
-        // If u has unvisited south neighbor, push south neighbor to neighbors
-        if (u.first / W < H - 1 && !v[u.first + W])
-            neighbors.push_back(u.first + W);
-        // If u has unvisited west neighbor, push west neighbor to neighbors
-        if (u.first % W > 0 && !v[u.first - 1])
-            neighbors.push_back(u.first - 1);
-        // If u has unvisited neighbors, push to the stack randomly, indicate random one as visited and add it to graph
+        if (u / W > 0 && !v[u - W])
+            neighbors.push_back(u - W);
+        if (u % W < W - 1 && !v[u + 1])
+            neighbors.push_back(u + 1);
+        if (u / W < H - 1 && !v[u + W])
+            neighbors.push_back(u + W);
+        if (u % W > 0 && !v[u - 1])
+            neighbors.push_back(u - 1);
+            
+        // Push neighbors onto the stack randomly
         if (!neighbors.empty()) {
             random_shuffle(neighbors.begin(), neighbors.end());
             for (auto neighbor : neighbors)
-                s.emplace(neighbor, u.first);
+                s.emplace(neighbor, u);
         }
     }
-    printGraph(adjList);
+    
+    // Convert adjacency list to bool matrix and print
+    auto matrix = toMatrix(adjList, H, W);
+    printMatrix(matrix);
 }
