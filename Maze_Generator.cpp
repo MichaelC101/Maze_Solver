fac211c11fb0d74c7f1a7275362cc916bd0ff41e
Sam Lonneman <56888420+SamLonneman@users.noreply.github.com>
2021-12-05 03:54:51 -0500

    Update Maze_Generator.cpp

--- Maze_Generator.cpp
+++ Maze_Generator.cpp
@@ -1,13 +1,13 @@
-#include <iostream>
+#include "Board.h"
+#include "TextureManager.h"
+#include <SFML/Graphics.hpp>
 #include <algorithm>
-#include <vector>
+#include <chrono>
+#include <iostream>
+#include <queue>
 #include <stack>
 #include <unordered_set>
-#include <chrono>
-#include <queue>
-#include <SFML/Graphics.hpp>
-#include "TextureManager.h"
-#include "Board.h"
+#include <vector>
 using namespace std;
 
 // Helper function prototypes
@@ -24,10 +24,13 @@ void setBoard(Board& board);
 int main()
 {
 	// 2500 x 40 gives 100k vertices and fits in console
-	const int Y = 2500, X = 40;
+	// const int Y = 2500, X = 40;
 	//auto bigAdjList = generateGraph(Y, X);
 	//auto bigMatrix = graphToMaze(bigAdjList, Y, X);
 	//printMaze(bigMatrix);
+
+	// Seed the randomizer
+	srand(time(0));
 
 	//SFML START
 	const int H = 25, W = 35;
@@ -39,26 +42,29 @@ int main()
 	sf::RenderWindow window(sf::VideoMode(1080, 720), "Maze Runner");
 	while (window.isOpen())
 	{
-		 sf::Event event;
-		 while (window.pollEvent(event))
-		 {
-			 if (event.type == sf::Event::Closed)
-				 window.close();
-			 if (event.type == sf::Event::MouseButtonPressed)
-			 {
-				 if (event.mouseButton.button == sf::Mouse::Left)
-				 {
-					 board.leftClick(sf::Mouse::getPosition(window), window, adjList, 0, 874);
-					 //71 3550 is converted values
-				 }
-			 }
-		 }
-		 window.clear(sf::Color(173,216,230,255));
-		 board.Draw(window);
-		 window.display();		
-	 }
-	 TextureManager::Clear();
-	 return 0;
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+			if (event.type == sf::Event::MouseButtonPressed)
+			{
+				// if (event.mouseButton.button == sf::Mouse::Right)
+				// {
+				// 	board.rightClick(sf::Mouse::getPosition(window));
+				// }
+				if (event.mouseButton.button == sf::Mouse::Left)
+				{
+					board.leftClick(sf::Mouse::getPosition(window), window, adjList, 0, 874);
+				}
+			}
+		}
+		window.clear(sf::Color(173, 216, 230, 255));
+		board.Draw(window);
+		window.display();
+	}
+	TextureManager::Clear();
+	return 0;
 }
 
 void setBoard(Board& board)
@@ -74,9 +80,8 @@ void setBoard(Board& board)
 	}
 }
 
-
 // Generates a random single-solution maze adjacency list using a version of DFS, then removes walls to introduce more solutions
-vector<unordered_set<int>> generateGraph(int H = 5, int W = 5)
+vector<unordered_set<int>> generateGraph(int H, int W)
 {
 	// Graph adjacency list
 	vector<unordered_set<int>> adjList(H * W);
@@ -89,7 +94,8 @@ vector<unordered_set<int>> generateGraph(int H = 5, int W = 5)
 	s.emplace(0, -1);
 
 	// Visits each vertex exactly once
-	while (!s.empty()) {
+	while (!s.empty())
+	{
 
 		// Get node to be visited and the node it is visited from
 		int u = s.top().first;
@@ -101,7 +107,8 @@ vector<unordered_set<int>> generateGraph(int H = 5, int W = 5)
 		// Otherwise, mark it as visited
 		v[u] = true;
 		// Excluding the first node, add the edge to the adjacency list
-		if (from != -1) {
+		if (from != -1)
+		{
 			adjList[from].insert(u);
 			adjList[u].insert(from);
 		}
@@ -118,7 +125,7 @@ vector<unordered_set<int>> generateGraph(int H = 5, int W = 5)
 			neighbors.push_back(u - 1);
 
 		// Push neighbors onto the stack in random order
-		if (!neighbors.empty()) 
+		if (!neighbors.empty())
 		{
 			random_shuffle(neighbors.begin(), neighbors.end());
 			for (auto neighbor : neighbors)
@@ -127,7 +134,8 @@ vector<unordered_set<int>> generateGraph(int H = 5, int W = 5)
 	}
 
 	// Remove some walls to create more solutions
-	for (int i = 0; i < max(H, W); i++) {
+	for (int i = 0; i < max(H, W); i++)
+	{
 		// Choose a random vertex
 		int u = rand() % (W * H);
 		// Create vector of *potential* neighbors of u
@@ -141,7 +149,7 @@ vector<unordered_set<int>> generateGraph(int H = 5, int W = 5)
 		if (u % W > 0 && !adjList[u].count(u - 1))
 			neighbors.push_back(u - 1);
 		// Knock down a random wall (make a connection with a random neighbor)
-		if (!neighbors.empty()) 
+		if (!neighbors.empty())
 		{
 			int v = neighbors[rand() % neighbors.size()];
 			adjList[u].insert(v);
@@ -157,7 +165,8 @@ vector<unordered_set<int>> generateGraph(int H = 5, int W = 5)
 vector<vector<bool>> graphToMaze(vector<unordered_set<int>>& adjList, int H, int W)
 {
 	vector<vector<bool>> matrix(H * 2 + 1, vector<bool>(W * 2 + 1));
-	for (int i = 0; i < adjList.size(); i++) {
+	for (unsigned int i = 0; i < adjList.size(); i++)
+	{
 		int r = i / W;
 		int c = i % W;
 		int row = r * 2 + 1;
@@ -173,29 +182,37 @@ vector<vector<bool>> graphToMaze(vector<unordered_set<int>>& adjList, int H, int
 	return matrix;
 }
 
-void printList(vector<unordered_set<int>>& adjList) {
-	for (int i = 0; i < adjList.size(); i++) {
-		if (!adjList[i].empty()) {
+void printList(vector<unordered_set<int>>& adjList)
+{
+	for (unsigned int i = 0; i < adjList.size(); i++)
+	{
+		if (!adjList[i].empty())
+		{
 			cout << i << " -> ";
-			for (auto it = adjList[i].begin(); it != adjList[i].end(); ++it) {
+			for (auto it = adjList[i].begin(); it != adjList[i].end(); ++it)
+			{
 				cout << *it << " ";
 			}
 			cout << endl;
 		}
 	}
 }
-
 //bellmanFord
-void bellmanFord(vector<unordered_set<int>>& adjList, int src, int end) {
+void bellmanFord(vector<unordered_set<int>>& adjList, int src, int end)
+{
 	auto start = chrono::high_resolution_clock::now();
 	vector<int> d(adjList.size(), 9999999);
 	vector<int> p(adjList.size(), -1);
 	d[src] = 0;
 	p[src] = 0;
-	for (int j = 0; j < adjList.size() - 1; j++) {
-		for (int i = 0; i < adjList.size(); i++) {
-			for (auto it = adjList[i].begin(); it != adjList[i].end(); ++it) {
-				if (d[*it] > d[i] + 1) {
+	for (unsigned int j = 0; j < adjList.size() - 1; j++)
+	{
+		for (unsigned int i = 0; i < adjList.size(); i++)
+		{
+			for (auto it = adjList[i].begin(); it != adjList[i].end(); ++it)
+			{
+				if (d[*it] > d[i] + 1)
+				{
 					d[*it] = d[i] + 1;
 					p[*it] = i;
 				}
@@ -208,18 +225,20 @@ void bellmanFord(vector<unordered_set<int>>& adjList, int src, int end) {
 	cout << endl;
 	cout << "Using Bellman-Ford" << endl;
 	cout << "Finding " << end << " from " << src << " using Bellman-Ford took "
-		<< duration.count() << " seconds" << endl;
+		 << duration.count() << " seconds" << endl;
 	cout << "Verticies visited " << d[end] << endl;
 	cout << "Best path from " << src << " to " << end << ": ";
 	int x = 999;
 	int y = end;
-	vector <int> v;
-	while (x != src) {
+	vector<int> v;
+	while (x != src)
+	{
 		x = p[y];
 		v.push_back(x);
 		y = x;
 	}
-	for (int i = v.size() - 1; i >= 0; i--) {
+	for (int i = v.size() - 1; i >= 0; i--)
+	{
 		cout << v[i] << " -> ";
 	}
 	cout << end << endl;
@@ -227,14 +246,17 @@ void bellmanFord(vector<unordered_set<int>>& adjList, int src, int end) {
 }
 
 // does dijkstra stuff
-void dijkstra(vector<unordered_set<int>>& adjList, int src, int end) {
+void dijkstra(vector<unordered_set<int>>& adjList, int src, int end)
+{
 	auto start = chrono::high_resolution_clock::now();
 	unordered_set<int> computed;
 	unordered_set<int> notComputed;
 	computed.emplace(src);
 	//filling notComputed with all verticies except src
-	for (int i = 0; i < adjList.size(); i++) {
-		if (computed.count(i) != 1) {
+	for (unsigned int i = 0; i < adjList.size(); i++)
+	{
+		if (computed.count(i) != 1)
+		{
 			notComputed.emplace(i);
 		}
 	}
@@ -244,15 +266,18 @@ void dijkstra(vector<unordered_set<int>>& adjList, int src, int end) {
 	// keeps track of predecessor
 	vector<int> p(adjList.size(), -1);
 
-	for (auto it = adjList[src].begin(); it != adjList[src].end(); ++it) {
+	for (auto it = adjList[src].begin(); it != adjList[src].end(); ++it)
+	{
 		d[*it] = 1;
 		p[*it] = src;
 	}
 
-	while (!notComputed.empty()) {
+	while (!notComputed.empty())
+	{
 		//need the smallest index that hasnt been visited yet
 		vector<int> temp = d;
-		for (auto it = computed.begin(); it != computed.end(); ++it) {
+		for (auto it = computed.begin(); it != computed.end(); ++it)
+		{
 			temp[*it] = 999999999;
 		}
 
@@ -260,42 +285,46 @@ void dijkstra(vector<unordered_set<int>>& adjList, int src, int end) {
 		computed.emplace(minIndex);
 		notComputed.erase(minIndex);
 
-		for (auto it = adjList[minIndex].begin(); it != adjList[minIndex].end(); ++it) {
-			if (notComputed.count(*it) != 0) {
-				if (d[minIndex] + 1 < d[*it]) {
+		for (auto it = adjList[minIndex].begin(); it != adjList[minIndex].end(); ++it)
+		{
+			if (notComputed.count(*it) != 0)
+			{
+				if (d[minIndex] + 1 < d[*it])
+				{
 					d[*it] = d[minIndex] + 1;
 					p[*it] = minIndex;
 				}
 			}
 		}
-
 	}
 	auto stopTime = chrono::high_resolution_clock::now();
 	chrono::duration<float> duration = stopTime - start;
 	cout << endl;
 	cout << "Using DIJKSTRA" << endl;
 	cout << "Finding " << end << " from " << src << " using DIJSKTRA took "
-		<< duration.count() << " Im not sure of time units" << endl;
+		 << duration.count() << " Im not sure of time units" << endl;
 	cout << "The distance from " << src << " to " << end << " is " << d[end] << endl;
 	cout << "Best path from " << src << " to " << end << ": ";
 	int x = 999;
 	int y = end;
-	vector <int> v;
-	while (x != src) {
+	vector<int> v;
+	while (x != src)
+	{
 		x = p[y];
 		v.push_back(x);
 		y = x;
 	}
-	for (int i = v.size() - 1; i >= 0; i--) {
+	for (int i = v.size() - 1; i >= 0; i--)
+	{
 		cout << v[i] << " -> ";
 	}
 	cout << end << endl;
 	cout << endl;
-
 }
 
-//DFS stuff 
-void DFS(vector<unordered_set<int>>& adjList, int src, int end) {
+//DFS stuff
+void DFS(vector<unordered_set<int>>& adjList, int src, int end)
+{
 	auto start = chrono::high_resolution_clock::now();
 	unordered_set<int> visited;
 	stack<int> stack;
@@ -303,20 +332,25 @@ void DFS(vector<unordered_set<int>>& adjList, int src, int end) {
 	stack.push(src);
 	vector<int> path;
 
-	while (!stack.empty()) {
+	while (!stack.empty())
+	{
 		int x = stack.top();
-		if (x == end) {
+		if (x == end)
+		{
 			break;
 		}
 		//cout << x << " ";
 		path.push_back(x);
 		stack.pop();
 		vector<int> neighbors;
-		for (auto it = adjList[x].begin(); it != adjList[x].end(); ++it) {
+		for (auto it = adjList[x].begin(); it != adjList[x].end(); ++it)
+		{
 			neighbors.push_back(*it);
 		}
-		for (int v : neighbors) {
-			if (visited.count(v) == 0) {
+		for (int v : neighbors)
+		{
+			if (visited.count(v) == 0)
+			{
 				visited.insert(v);
 				stack.push(v);
 			}
@@ -326,17 +360,19 @@ void DFS(vector<unordered_set<int>>& adjList, int src, int end) {
 	chrono::duration<float> duration = stopTime - start;
 	cout << "Using Depth First Search" << endl;
 	cout << "Finding " << end << " from " << src << " using DFS took "
-		<< duration.count() << " Im not sure time unit" << endl;
+		 << duration.count() << " Im not sure time unit" << endl;
 	cout << "The distance from " << src << " to " << end << " is " << path.size() << endl;
 	cout << "Best path from " << src << " to " << end << ": ";
-	for (int i = 0; i < path.size(); i++) {
+	for (unsigned int i = 0; i < path.size(); i++)
+	{
 		cout << path[i] << "-> ";
 	}
 	cout << end;
 }
 
 //BFS search
-void BFS(vector<unordered_set<int>>& adjList, int src, int end) {
+void BFS(vector<unordered_set<int>>& adjList, int src, int end)
+{
 	auto start = chrono::high_resolution_clock::now();
 	unordered_set<int> visited;
 	queue<int> queue;
@@ -344,19 +380,24 @@ void BFS(vector<unordered_set<int>>& adjList, int src, int end) {
 	queue.push(src);
 	vector<int> path;
 
-	while (!queue.empty()) {
+	while (!queue.empty())
+	{
 		int x = queue.front();
-		if (x == end) {
+		if (x == end)
+		{
 			break;
 		}
 		path.push_back(x);
 		queue.pop();
 		vector<int> neighbors;
-		for (auto it = adjList[x].begin(); it != adjList[x].end(); ++it) {
+		for (auto it = adjList[x].begin(); it != adjList[x].end(); ++it)
+		{
 			neighbors.push_back(*it);
 		}
-		for (int v : neighbors) {
-			if (visited.count(v) == 0) {
+		for (int v : neighbors)
+		{
+			if (visited.count(v) == 0)
+			{
 				visited.insert(v);
 				queue.push(v);
 			}
@@ -368,27 +409,12 @@ void BFS(vector<unordered_set<int>>& adjList, int src, int end) {
 	cout << endl;
 	cout << "Using Breadth First Search" << endl;
 	cout << "Finding " << end << " from " << src << " using BFS took "
-		<< duration.count() << " Im not sure time unit" << endl;
+		 << duration.count() << " Im not sure time unit" << endl;
 	cout << "The distance from " << src << " to " << end << " is " << path.size() << endl;
 	cout << "Best path from " << src << " to " << end << ": ";
-	for (int i = 0; i < path.size(); i++) {
+	for (unsigned int i = 0; i < path.size(); i++)
+	{
 		cout << path[i] << "-> ";
 	}
 	cout << end;
-}
-
-// Convenient way to view a bool matrix maze without external libraries
-void printMaze(vector<vector<bool>>& matrix)
-{
-	char path = 255u;
-	char wall = 219u;
-	for (auto v : matrix) {
-		for (auto b : v) {
-			if (b)
-				cout << path << path;
-			else
-				cout << wall << wall;
-		}
-		cout << endl;
-	}
 }
