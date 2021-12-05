fc8e381ba5292bb050a76a5a01df87e9f42db59e
AlexOlson14 <73795705+AlexOlson14@users.noreply.github.com>
2021-12-05 01:20:06 -0500

    Update Board.h

--- Board.h
+++ Board.h
@@ -23,12 +23,14 @@ class Board
 public:
 	Board();
 	void Draw(sf::RenderWindow& window);
-	void rightClick(sf::Vector2i mousePos);
 	void leftClick(sf::Vector2i mousePos, sf::RenderWindow& window, vector<unordered_set<int>>& adjList, int src, int end);
 	void resetGame();
 	void addTile(Tile& tile, int x, int y);
 	void setMaze(vector<vector<bool>> matrix);
 	void runBFS(sf::RenderWindow& window, vector<unordered_set<int>>& adjList, int src, int end);
+	void runDFS(sf::RenderWindow& window, vector<unordered_set<int>>& adjList, int src, int end);
+	void runDjikstra(sf::RenderWindow& window, vector<unordered_set<int>>& adjList, int src, int end);
+	void runBellmanFord(sf::RenderWindow& window, vector<unordered_set<int>>& adjList, int src, int end);
 	void reDraw(sf::RenderWindow& window, int x, int y);
 };
 
