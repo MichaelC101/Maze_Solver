8f6a68bf41056f84845cacb5cb6bc2a944be65cf
Sam Lonneman <56888420+SamLonneman@users.noreply.github.com>
2021-12-09 00:00:40 -0500

    Cleaned up old stuff

--- Main.cpp
+++ Main.cpp
@@ -10,21 +10,8 @@
 #include <vector>
 using namespace std;
 
-// Helper function prototypes
-vector<unordered_set<int>> generateGraph(int H, int W);
-vector<vector<bool>> graphToMaze(vector<unordered_set<int>>& adjList, int H, int W);
-void printMaze(vector<vector<bool>>& matrix);
-void printList(vector<unordered_set<int>>& adjList);
-
-// Program Driver
 int main()
 {
-	// 2500 x 40 gives 100k vertices and fits in console
-	// const int Y = 2500, X = 40;
-	//auto bigAdjList = generateGraph(Y, X);
-	//auto bigMatrix = graphToMaze(bigAdjList, Y, X);
-	//printMaze(bigMatrix);
-
 	// Seed the randomizer
 	srand(time(0));
 
@@ -53,20 +40,3 @@ int main()
 	TextureManager::Clear();
 	return 0;
 }
-
-void printList(vector<unordered_set<int>>& adjList)
-{
-	for (unsigned int i = 0; i < adjList.size(); i++)
-	{
-		if (!adjList[i].empty())
-		{
-			cout << i << " -> ";
-			for (auto it = adjList[i].begin(); it != adjList[i].end(); ++it)
-			{
-				cout << *it << " ";
-			}
-			cout << endl;
-		}
-	}
-}
-
